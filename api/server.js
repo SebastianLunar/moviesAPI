554b3cc2571561866c7cb0b4fa97e5fd5632b487
Sebastian Luna <selunar@outlook.com>
2024-07-23 22:18:53 -0500

    probando3

--- api/server.js
+++ api/server.js
@@ -1,17 +1,19 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/product/:resource/:id/show' : '/resource/:id'
+    '/api/*': '/$1',
+    '/product/:resource/:id/show': '/:resource/:id'
 }))
-server.use(router);
+server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running on port 3000');
-});
+    console.log('JSON Server is running on port 3000')
+})
 
-module.exports = server;
+// Export the Server API
+module.exports = server
